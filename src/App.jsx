8268d12f9411df0e4916dc76b7a7f9a98caa1f6a
Kevin Fabian Garcés Martínez <kgarces@eci.com.co>
2023-08-02 11:18:59 -0500

    Quinto intento

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
 import Work from "./components/Work";
 import Projects from "./components/Projects";
-import React from 'react';
+
 
 
 function App() {
--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'react-icons/fa', // Agrega aquí todos los íconos que utilices en tu proyecto
-        'react-icons/ai',
-        'react-icons/bs',
-        'react-icons/gr',
-        // Agrega más íconos aquí si los necesitas
-      ],
-    },
-  },
 })
