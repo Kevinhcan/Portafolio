3180d49af9818ee66bae052806851c77dacba428
Kevin Fabian Garcés Martínez <kgarces@eci.com.co>
2023-08-02 10:59:34 -0500

    Tercer intento

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
 import Work from "./components/Work";
 import Projects from "./components/Projects";
+import React from 'react';
+
 
 function App() {
   return (
