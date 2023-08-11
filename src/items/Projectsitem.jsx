42bde5ff952d04380514b5dfdcc71ceb1a6a876b
Kevin Fabian Garcés Martínez <kgarces@eci.com.co>
2023-08-11 08:37:15 -0500

    bugs

--- src/items/Projectsitem.jsx
+++ src/items/Projectsitem.jsx
@@ -4,7 +4,7 @@ const Projectsitem = ({ img, title }) => {
  
   return (
     <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-500 rounded-xl group dark:hover:bg-gradient-to-br dark:from-purple-600 dark:to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 /// bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-lime-500 via-teal-600 to-indigo-900 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 hover:scale-110 transition-all duration-300">
-      <img src={img} alt="/" className="rounded-xl group-hover:opacity-10" />
+      <img src={img} alt="/" className="rounded-xl h-[300px] group-hover:opacity-10" />
       <div className="hidden group-hover:block  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
         <h3 className="text-2xl font-bold text-white tracking-wider text-center">
           {title}
